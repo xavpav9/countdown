3f94e7436b5f3c68913f936de579f9759cf7ae84
Xavier Tondelier <xtondelier@gmail.com>
2025-09-12 13:46:16 +0100

    Make chosen numbers display and add limitation to numbers

--- script.js
+++ script.js
@@ -2,29 +2,44 @@ const gameBoard = document.querySelector(".gameboard");
 const smallRows = document.querySelectorAll(".small-row");
 const largeRow = document.querySelector(".large-row");
 const selection = document.querySelector(".selection");
+const chosen = document.querySelector(".chosen");
 
-const smallRowsValues = []
-const largeRowValues = []
+const display = [];
+const rowValues = [];
+assignNumbers();
+
+selection.addEventListener("click", evt => {
+  const boxes = [...selection.querySelectorAll(".large-row > div, .small-row > div")];
+  if (boxes.includes(evt.target) && ![...evt.target.classList].includes("selected") && display.length < 6) {
+    evt.target.classList.add("selected");
+    display.push(rowValues[boxes.indexOf(evt.target)]);
+    displayValues();
+  };
+});
 
 function assignNumbers() {
   const smallOptions = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10]
   const largeOptions = [25,50,75,100]
-  for (let i = 0; i < 20; ++i) {
+  for (let i = 4; i < 24; ++i) {
     index = Math.floor(Math.random() * smallOptions.length);
-    smallRowsValues[i] = smallOptions[index];
+    rowValues[i] = smallOptions[index];
     smallOptions.splice(index, 1)
-  }
+  };
 
   for (let i = 0; i < 4; ++i) {
     index = Math.floor(Math.random() * largeOptions.length);
-    largeRowValues[i] = largeOptions[index];
+    rowValues[i] = largeOptions[index];
     largeOptions.splice(index, 1)
-  }
-}
+  };
+};
 
-selection.addEventListener("click", evt => {
-  if ([...selection.querySelectorAll(".large-row > div, .small-row > div")].includes(evt.target)) {
-    evt.target.classList.add("selected");
-  }
-});
+function displayValues() {
+  const displayBoxes = [...chosen.querySelectorAll("div")];
+  for (let i = 0; i < 6; ++i) {
+    if (display[i] !== undefined) {
+      displayBoxes[i].textContent = display[i];
+      if (display[i] === 100) displayBoxes[i].classList.add("hundred");
+    } else break;
+  };
+};
 
